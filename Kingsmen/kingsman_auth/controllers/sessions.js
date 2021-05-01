249185f9a2400c68e9db62613b9ca18ac008594a
jcari-rgb <582119jc@gmail.com>
2021-04-30 23:43:36 -0400

    login works now

--- Kingsmen/kingsman_auth/controllers/sessions.js
+++ Kingsmen/kingsman_auth/controllers/sessions.js
@@ -7,10 +7,15 @@ router.get('/', (req, res) => {
     res.render('../views/sessions/new.ejs')
 })
 
-router.post('/new', (req, res) => {
-    // const hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-    db.users.find({})
-    console.log(req.body.password)
+router.post('/', (req, res) => {
+    User.find({ "username": req.body.username }, (err, checkUser) => {
+        console.log(foundUser)
+        if (req.body.password === checkUser[0].password) {
+            res.redirect('/room')
+        } else {
+            res.send('login failed')
+        }
+    })
 })
 
 module.exports = router;
